8b9078e6dfccb779d1c964fc67efadf75b6de023
~.~`` <hy15816@qq.com>
2016-07-06 10:33:02 +0800

    lost-souls
    1.嵌套scrollView手势冲突
    2.半自定义 textField

---
+++ TestScrollTests/TestScrollTests.m
@@ -0,0 +1,39 @@
+//
+//  TestScrollTests.m
+//  TestScrollTests
+//
+//  Created by Lost_souls on 16/6/29.
+//  Copyright © 2016年 __lost_souls. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestScrollTests : XCTestCase
+
+@end
+
+@implementation TestScrollTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
